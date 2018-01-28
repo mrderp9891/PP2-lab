f9a46dc6bcbc9098e2f5d41cec23a75fc8d4283a
Madi <bomi1337@gmail.com>
2018-01-28 22:22:02 +0600

    lab2

---
+++ lab2/3_Complex/3_Complex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_Complex
+{
+
+    class Complex
+        {
+            public void C1(int a, int b)
+            {
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            }
+             public void C1(int a, int b, int c)
+            {
+            Console.WriteLine(a + " + " + b + " + " + c + " = " + (a + b + c));
+            }
+    }
+    
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Complex Com = new Complex();
+            Com.C1(4, 7);
+            Com.C1(5, 3, 4);
+            Console.ReadKey();
+        }
+    }
+}
